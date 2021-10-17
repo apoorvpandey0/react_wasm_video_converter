6cb5efb3ddb5184f2570805f60e78d77d70b53f0
apoorvpandey0 <apoorvpandey0@gmail.com>
2021-10-17 12:53:31 +0530

    convert to gif working

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ const ffmpeg = createFFmpeg({ log: true });
 function App() {
 const [ready, setReady] = useState(false);
 const [video, setVideo] = useState();
+const [gif, setGif] = useState();
 
 const load = async () => {
   await ffmpeg.load();
@@ -17,20 +18,38 @@ useEffect(() => {
   load();
 }, [])
 
+const convertToGif = async () => {
+  // Write the file to memory 
+  ffmpeg.FS('writeFile', 'test.mp4', await fetchFile(video));
 
+  // Run the FFMpeg command
+  await ffmpeg.run('-i', 'test.mp4', '-t', '2.5', '-ss', '2.0', '-f', 'gif', 'out.gif');
+
+  // Read the result
+  const data = ffmpeg.FS('readFile', 'out.gif');
+
+  // Create a URL
+  const url = URL.createObjectURL(new Blob([data.buffer], { type: 'image/gif' }));
+  setGif(url)
+} 
 
  return ready? 
   <div className="App">
-          { video && <video
+    <h2>Video file to GIF converter</h2>
+      
+      { video && <video
         controls
         width="250"
         src={URL.createObjectURL(video)}>
-
       </video>}
-
 
       <input type="file" onChange={(e) => setVideo(e.target.files?.item(0))} />
 
+      <h3>Result</h3>
+
+      <button onClick={convertToGif}>Convert</button>
+
+      { gif && <img src={gif} width="250" />}
   </div>:
   <div>Loading...</div>;
 }
